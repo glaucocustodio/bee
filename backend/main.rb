c54c7241ad559227f6098de06b7ea72b44511ed9
Glauco Custódio <glauco.custodio@gmail.com>
2020-11-19 17:34:30 +0000

    add .env file

--- backend/main.rb
+++ backend/main.rb
@@ -1,11 +1,12 @@
 require "sinatra/base"
 require 'sinatra/cors'
+require 'dotenv/load'
 require_relative 'password_checker'
 
 class Backend < Sinatra::Base
   register Sinatra::Cors
 
-  set :allow_origin, "http://localhost:3000"
+  set :allow_origin, ENV['CORS_ORIGIN']
   set :allow_methods, "GET,HEAD,POST"
   set :allow_headers, "content-type,if-modified-since"
 
